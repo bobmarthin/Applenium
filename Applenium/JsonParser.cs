5dda2f58035c9e86cade0c21f5dbb94f006be06d
Maxim Guenis <maximguenis@gmail.com>
2013-09-22 15:06:24 +0300

    FirstTime Commit ver 1.0
    This is first time I'm publishing this tool. The development of this
    tool was made during my work in eToro. Therefore all credentials saved
    to eToro. I removed all confidential data from the code, but still there
    are  some  not confidential leftovers like eToro icons or old tool name
    (a.k.a eToroAutoStudio).

---
+++ Applenium/JsonParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Applenium
+{
+    internal class JsonParser
+    {
+        public string ReadJson(string parameterName)
+        {
+            if (parameterName == null) throw new ArgumentNullException("parameterName");
+            string text = File.ReadAllText("Configuration.json");
+            JObject json = JObject.Parse(text);
+            var parameterValue = (string) json[parameterName];
+            return parameterValue;
+        }
+
+        public DataTable ReadJsonToDt()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Value");
+
+            string text = File.ReadAllText("Applenium.json");
+            JObject json = JObject.Parse(text);
+            foreach (var jv in json)
+            {
+                var data = new[] {jv.Key, jv.Value.ToString()};
+                dt.Rows.Add(data);
+            }
+            return dt;
+        }
+
+        public void WriteJson(string name, string value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            string text = File.ReadAllText("Configuration.json");
+            JObject json = JObject.Parse(text);
+            json[name] = value;
+            File.WriteAllText("Configuration.json", json.ToString());
+        }
+    }
+}
---
+++ Applenium/Logger.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+
+namespace Applenium
+{
+    /// <summary>
+    ///     Logger - public class to deal with Logs
+    /// </summary>
+    public static class Logger
+    {
+        private static readonly ILog Ilogger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly bool IsDebugEnabled;
+        private static readonly bool IsInfoEnabled;
+        private static readonly bool IsWarnEnabled;
+        private static readonly bool IsErrorEnabled;
+        private static readonly bool IsFatalEnabled;
+
+        static Logger()
+        {
+            XmlConfigurator.Configure();
+            IsDebugEnabled = Ilogger.IsDebugEnabled;
+            IsInfoEnabled = Ilogger.IsInfoEnabled;
+            IsWarnEnabled = Ilogger.IsWarnEnabled;
+            IsErrorEnabled = Ilogger.IsErrorEnabled;
+            IsFatalEnabled = Ilogger.IsFatalEnabled;
+        }
+
+        /// <summary>
+        ///     Debug is public function to report debug events
+        /// </summary>
+        public static void Debug(string message, Exception ex = null)
+        {
+            if (!IsDebugEnabled)
+            {
+                return;
+            }
+            if (ex == null)
+            {
+                Ilogger.Debug(message);
+            }
+            else
+            {
+                Ilogger.Debug(message, ex);
+            }
+        }
+
+        /// <summary>
+        ///     Info is public function to report Information events
+        /// </summary>
+        public static void Info(string message, Exception ex = null)
+        {
+            if (!IsInfoEnabled)
+            {
+                return;
+            }
+            if (ex == null)
+            {
+                using (ThreadContext.Stacks["Status"].Push("[INFO]"))
+                {
+                    Ilogger.Info(message);
+                }
+            }
+            else
+            {
+                Ilogger.Info(message, ex);
+            }
+        }
+
+        /// <summary>
+        ///     public function to report warnings
+        /// </summary>
+        public static void Warn(string message, Exception ex = null)
+        {
+            if (!IsWarnEnabled)
+            {
+                return;
+            }
+            if (ex == null)
+            {
+                using (ThreadContext.Stacks["Status"].Push("[WARN]"))
+                {
+                    Ilogger.Warn(message);
+                }
+            }
+            else
+            {
+                Ilogger.Warn(message, ex);
+            }
+        }
+
+        /// <summary>
+        ///     is public function to report error events
+        /// </summary>
+        public static void Error(string message, Exception ex = null)
+        {
+            if (!IsErrorEnabled)
+            {
+                return;
+            }
+            if (ex == null)
+            {
+                using (ThreadContext.Stacks["Status"].Push("[ERROR]"))
+                {
+                    Ilogger.Error(message);
+                }
+            }
+            else
+            {
+                Ilogger.Error(message, ex);
+            }
+        }
+
+        /// <summary>
+        ///     Debug is public function to report fatal events
+        /// </summary>
+        public static void Fatal(string message, Exception ex = null)
+        {
+            if (!IsFatalEnabled)
+            {
+                return;
+            }
+            if (ex == null)
+            {
+                using (ThreadContext.Stacks["Status"].Push("[FATAL]"))
+                {
+                    Ilogger.Fatal(message);
+                }
+
+            }
+            else
+            {
+                Ilogger.Fatal(message, ex);
+            }
+        }
+
+        /// <summary>
+        ///     To report passed test/step or scenario
+        /// </summary>
+        public static void Passed(string message)
+        {
+            if (!IsInfoEnabled)
+            {
+                return;
+            }
+            using (ThreadContext.Stacks["Status"].Push("[PASSED]"))
+            {
+                Ilogger.Info(message);
+            }
+        }
+
+        /// <summary>
+        ///     To report failed test/step or scenario
+        /// </summary>
+        public static void Failed(string message)
+        {
+            if (!IsErrorEnabled)
+            {
+                return;
+            }
+            using (ThreadContext.Stacks["Status"].Push("[FAILED]"))
+            {
+                Ilogger.Error(message);
+            }
+        }
+
+        /// <summary>
+        ///     To report Done passed test/step or scenario
+        /// </summary>
+        public static void Done(string message)
+        {
+            if (!IsInfoEnabled)
+            {
+                return;
+            }
+            using (ThreadContext.Stacks["Status"].Push("[DONE]"))
+            {
+                Ilogger.Info(message);
+            }
+        }
+    }
+}
