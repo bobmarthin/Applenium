3510c2c8865296b232fd505edbfdd113b3d92c9c
Maxim Guenis <maximguenis@gmail.com>
2013-10-08 15:34:07 +0300

    Version 1.1
    Android support

---
+++ UnitTestExecutionManager/bin/Release/DataSetAutoTest.cs
@@ -0,0 +1,64 @@
+﻿namespace WPFeToroAutoStudio
+{
+    public partial class DataSetAutoTest
+    {
+        partial class GuiMapDataTable
+        {
+        }
+
+        partial class GuiPageSectionDataTable
+        {
+        }
+
+        partial class GuiScenarioLogicDataTable
+        {
+        }
+
+        partial class GuiTestStepsDataTable
+        {
+        }
+    }
+}
+
+namespace WPFeToroAutoStudio.DataSetAutoTestTableAdapters
+{
+    partial class GuiTagTypeTableAdapter
+    {
+    }
+
+    partial class BatchLogicTableAdapter
+    {
+    }
+
+    partial class GuiPageSectionTableAdapter
+    {
+    }
+
+    partial class TestTableAdapter
+    {
+    }
+
+    partial class BatchesTableAdapter
+    {
+    }
+
+    partial class GuiScenarioLogicTableAdapter
+    {
+    }
+
+    partial class GuiMapTableAdapter
+    {
+    }
+
+    partial class ScenarioTableAdapter
+    {
+    }
+
+    partial class GuiProjectPageTableAdapter
+    {
+    }
+
+    public partial class GuiTestStepsTableAdapter
+    {
+    }
+}
